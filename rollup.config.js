7f324a2dd241cabf7b2e17352fb954da3cc524e6
Jacob Nielsen <jacob@sitetech.dk>
2020-02-19 10:16:26 +0100

    svelte start

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,9 @@
-import svelte from 'rollup-plugin-svelte';
-import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
+import svelte from 'rollup-plugin-svelte';
 import { terser } from 'rollup-plugin-terser';
+import { autoPreprocess } from 'svelte-preprocess/dist/autoProcess';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -22,7 +23,8 @@ export default {
 			// a separate file - better for performance
 			css: css => {
 				css.write('public/build/bundle.css');
-			}
+			},
+			preprocess: autoPreprocess()
 		}),
 
 		// If you have external dependencies installed from
--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,22 @@
 <script>
-	export let name;
+
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+<style type="text/scss">
+  :global(body) {
+    background-color: #212121;
+  }
+  span {
+    display: inline-flex;
+    .green {
+      color: #66bb6a;
+    }
+  }
 </style>
+
+<main>
+  <span>
+    <h1 class="green">niller</h1>
+    <h1>.app</h1>
+  </span>
+</main>
